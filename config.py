d3593fb7492087afcc47d47b4a5159feeb23eb88
ryu-matsu0624 <ryu.nosuke.iphone@gmail.com>
2025-04-13 12:12:17 +0900

    fix: SECRET_KEY 読み込み追加

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-from dotenv import load_dotenv
+# config.py
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    PIXABAY_API_KEY = os.getenv("PIXABAY_API_KEY")
