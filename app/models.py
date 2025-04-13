e2a7417b3a5d03d31451cc71a98b0f312f30843a
ryu-matsu0624 <ryu.nosuke.iphone@gmail.com>
2025-04-13 17:02:55 +0900

    🔧 Extend password_hash to 255 chars & migrate

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(225), nullable=False)
 
     is_active_flag = db.Column(db.Boolean, default=True)
 
