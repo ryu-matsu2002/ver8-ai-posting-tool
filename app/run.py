c8b75f82729b57d6ffe13ee82d4b933cdbe8be9e
ryu-matsu0624 <ryu.nosuke.iphone@gmail.com>
2025-04-13 16:12:46 +0900

    マイグレーション追加＆モデル修正

--- app/run.py
+++ app/run.py
@@ -2,9 +2,13 @@ from app import create_app
 from app.extensions import db
 from flask_migrate import Migrate
 from app.scheduler import scheduler
+from flask_migrate import upgrade
 
 app = create_app()
 migrate = Migrate(app, db)
+
+with app.app_context():
+    upgrade()
 
 scheduler.init_app(app)
 scheduler.start()
